a36de014bc8ac99bb4931d716aeeaa6a5d1a27ee
Adam Bryant <eranegera@gmail.com>
2016-11-02 18:33:19 -0700

    Add initial project files.

---
+++ Assets/Project/Scripts/Test.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+	private void Awake()
+	{
+		AsyncIO.ForceDotNet.Force();
+	}
+
+	// Use this for initialization
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+}
